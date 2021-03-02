5c4c713889890587378a9edf0b4a1f4f02939c2e
Chamika THALAGALAGE <chamika_thalagalage@hotmail.fr>
2021-03-02 13:25:01 +0100

    Create LoginActivity.java
    Login Activity

---
+++ BDD/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.chamikathalagalage.loginregisterv2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText pseudo,password;
+    TextView versRegisterbtn;
+    Button btn_login;
+    String insertUrl = "http://192.168.56.1/mangaReader/loginUtilisateur.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        pseudo = (EditText) findViewById(R.id.pseudo);
+        password = (EditText) findViewById(R.id.password);
+        versRegisterbtn = (TextView) findViewById(R.id.versRegisterbtn);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+
+        versRegisterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+
+    public void Login() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.POST, insertUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.trim().equals("success")){
+                    startActivity(new Intent(getApplicationContext(), WelcomeActivity.class));
+                    Toast.makeText(getApplicationContext(), "Login successful!",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Login failed!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "error" + error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parameters  = new HashMap<String, String>();
+                parameters.put("pseudo",pseudo.getText().toString().trim());
+                parameters.put("password",password.getText().toString().trim());
+
+                return parameters;
+            }
+        };
+        requestQueue.add(request);
+
+    }
+
+}
